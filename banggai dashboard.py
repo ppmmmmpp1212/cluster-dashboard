8274524826c40828e552f7256acbc6f71faf9df4
ppmmmmpp1212 <analyticsmmpp@gmail.com>
2026-03-05 11:43:59 +0800

    Update banggai dashboard.py

--- banggai dashboard.py
+++ banggai dashboard.py
@@ -12,7 +12,7 @@ def run_app():
         "Stock Monitoring": "https://lookerstudio.google.com/embed/reporting/a83b2cc3-b544-4af4-aa1f-69c855927bf3/page/YzMZE",
         "NGRS Monitoring": "https://lookerstudio.google.com/embed/reporting/7bee7469-1bba-4f25-8ddb-5db385ef8e59/page/RAUQF",
         "SF KPI Monioring": "https://lookerstudio.google.com/embed/reporting/7d238133-e1f5-4404-8f9e-88022ba8a6b5/page/p_k6il67mwnd",
-        "Sales Analysis: "https://lookerstudio.google.com/embed/reporting/ddb47467-7f78-41a1-b14e-53f9d8ba83f9/page/3pUkF"
+        "Sales Analysis": "https://lookerstudio.google.com/embed/reporting/ddb47467-7f78-41a1-b14e-53f9d8ba83f9/page/3pUkF"
     }
 
     # Menu sidebar menggunakan streamlit-option-menu
@@ -58,6 +58,7 @@ def run_app():
 
 if __name__ == "__main__":
     run_app()
+
 
 
 
